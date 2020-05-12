f53f41865ae3b5d8fe3ba3d7b304eb38e762d4b9
RicardoRosal01 <ricardo.rosal01@gmail.com>
2020-05-12 13:30:32 -0300

    correção da rota de adicição de axioma

--- rotas/axiomas.js
+++ rotas/axiomas.js
@@ -3,18 +3,43 @@ const router = express.Router()
 const Axioma = require('../modelos/axiomas')
 
 //trazer todos os axiomas
-router.get('/',(req,res) => {
-    res.render('index')
+router.get('/',async (req,res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.nome !== '') {
+        searchOptions.nome = new RegExp(req.query.nome,'i')
+    }
+    try{
+        const axiomas = await Axioma.find(searchOptions)
+        res.render('axioma/index',{
+            axiomas: axiomas,
+            searchOptions:req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+
 })
 
 //novo axioma
 router.get('/novo',(req,res) => {
-    res.render('index')
+    res.render('axioma/novo',{axioma: new Axioma()})
 })
 
 //inserir axioma
-router.post('/novo',(req,res) => {
-    res.render('index')
+router.post('/', async (req,res) => {
+    const axioma = new Axioma({
+        nome: req.body.nome,
+        declaracao: req.body.declaracao
+    })
+    try {
+            const novoAxioma = await axioma.save()
+            res.redirect(`axioma/${novoAxioma.id}`)
+    } catch {
+            res.render('axioma/novo',{
+                axioma: axioma,
+                errorMessage: 'Erro ao declarar um novo axioma'
+            })
+    }
 })
 
 module.exports = router
